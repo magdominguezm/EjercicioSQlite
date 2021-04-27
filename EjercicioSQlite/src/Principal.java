321842c8aee8521cd1c0298642c634f085241b21
magdominguezm <magdominguezm@gmail.com>
2021-04-27 10:11:57 +0200

    Líneas añadidas en el método main() de la clase Principal

--- EjercicioSQlite/src/Principal.java
+++ EjercicioSQlite/src/Principal.java
@@ -71,6 +71,8 @@ public class Principal extends JFrame implements ActionListener{
 
 	public static void main(String[] args) {
 		 new Principal();
+		 System.out.println ("Línea 1 añadida en método main() de la clase Principal");
+		 System.out.println ("Línea 2 añadida en método main() de la clase Principal");
 		}//fin main
    
 }//fin class 
